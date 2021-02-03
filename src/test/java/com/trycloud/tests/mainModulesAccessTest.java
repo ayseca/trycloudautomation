f3348f7d83e2ba481ebd219f0fb2ae6d2a680a04
Khorsov <hhorsovv@gmail.com>
2021-02-03 10:46:01 -0500

    Main modules access Test commit

---
+++ src/test/java/com/trycloud/tests/mainModulesAccessTest.java
@@ -0,0 +1,60 @@
+package com.trycloud.tests;
+
+import com.trycloud.pages.AllFilesPage;
+import com.trycloud.tests.base.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class mainModulesAccessTest extends TestBase {
+    @Test
+    public void verifyMainModulesAccessFiles(){
+
+        AllFilesPage allModules = new AllFilesPage();
+
+        String expectedTitleFiles = allModules.expectedTitleFiles;
+        String filesGetActualTitle = allModules.filesGetActualTitle();
+        Assert.assertEquals(filesGetActualTitle,expectedTitleFiles, "We have access to Files");
+
+    }
+    @Test
+    public void verifyMainModulesAccessActivities(){
+
+        AllFilesPage allModules = new AllFilesPage();
+
+        String expectedTitleActivities = allModules.expectedTitleActivities;
+        String activityGetActualTitle = allModules.activityGetActualTitle();
+        Assert.assertEquals(activityGetActualTitle,expectedTitleActivities, "We have access to Activities");
+
+    }
+    @Test
+    public void verifyMainModulesAccessGallery(){
+
+        AllFilesPage allModules = new AllFilesPage();
+
+        String expectedTitleGallery = allModules.expectedTitleGallery;
+        String galleryGetActualTitle = allModules.galleryGetActualTitle();
+        Assert.assertEquals(galleryGetActualTitle,expectedTitleGallery, "We have access to Gallery");
+
+    }
+    @Test
+    public void verifyMainModulesAccessTalk(){
+
+        AllFilesPage allModules = new AllFilesPage();
+
+        String expectedTitleTalk = allModules.expectedTitleTalk;
+        String talkGetActualTitle = allModules.talkGetActualTitle();
+        Assert.assertEquals(talkGetActualTitle,expectedTitleTalk, "We have access to Talk");
+
+    }
+    @Test
+    public void verifyMainModulesAccessCalendar(){
+
+        AllFilesPage allModules = new AllFilesPage();
+
+        String expectedTitleCalendar = allModules.expectedTitleCalendar;
+        String calendarGetActualTitle = allModules.calendarGetActualTitle();
+        Assert.assertEquals(calendarGetActualTitle,expectedTitleCalendar, "We have access to Calendar");
+
+    }
+
+}
