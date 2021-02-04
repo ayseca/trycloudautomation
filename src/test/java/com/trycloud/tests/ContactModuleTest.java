3491079c49225e071f514406be8f07e16805eab2
EvaBar <romevgbarb0425@gmail.com>
2021-02-04 08:48:33 -0500

    ContactPage and ContactModuleTest classes created

--- src/test/java/com/trycloud/tests/ContactModuleTest.java
+++ src/test/java/com/trycloud/tests/ContactModuleTest.java
@@ -1,14 +1,45 @@
 package com.trycloud.tests;
 
+import com.trycloud.pages.ContactPage;
 import com.trycloud.pages.HomePage;
 import com.trycloud.tests.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import javax.xml.xpath.XPath;
 
 public class ContactModuleTest extends TestBase {
 
-    HomePage contactModule = new HomePage();
 
 
+    @Test
+
+    public void test(){
+
+        HomePage contactModule = new HomePage();
+
+        ContactPage contactPage = new ContactPage();
+
+        contactModule.ContactModule();
+
+        contactPage.click();
+
+        contactPage.createContact();
+
+        Assert.assertTrue(contactPage.NewContactNameDisplayed());
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
