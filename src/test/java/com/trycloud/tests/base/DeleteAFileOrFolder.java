92037aa6a39c9639b3460c9e2d6f7ba37a77a349
nilufaranwar <nilox_@hotmail.com>
2021-02-04 00:31:33 -0500

    US3,TC8

--- src/test/java/com/trycloud/tests/base/DeleteAFileOrFolder.java
+++ src/test/java/com/trycloud/tests/base/DeleteAFileOrFolder.java
@@ -1,10 +1,22 @@
 package com.trycloud.tests.base;
 
+import com.trycloud.pages.base.DeletePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DeleteAFileOrFolder extends TestBase{
 
+@Test
+    public void deleteFile(){
 
+    DeletePage deletePage = new DeletePage();
+    deletePage.DeleteFile();
+    Assert.assertTrue(deletePage.fileDisplay());
+
+
+
+
+    }
 
 
 
