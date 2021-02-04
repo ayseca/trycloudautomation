a80083b05fe9644cc29a0fee20f70ab253e127fe
umeda Davlatova <umeda.davlatova@gmail.com>
2021-02-04 12:27:13 -0500

    Note pages created !!!

--- src/test/java/com/trycloud/tests/NotesModule.java
+++ src/test/java/com/trycloud/tests/NotesModule.java
@@ -1,13 +1,9 @@
 package com.trycloud.tests;
 
 import com.trycloud.pages.AllFilesPage;
+import com.trycloud.pages.NotePage;
 import com.trycloud.tests.base.TestBase;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class NotesModule extends TestBase {
@@ -20,15 +16,18 @@ public class NotesModule extends TestBase {
     }
     @Test
     public void noteAddTest(){
-        driver.findElement(By.xpath("//*[@id='appmenu']/li[7]")).click();
-        // identifying the add note button
-        WebElement addNote = driver.findElement(By.xpath("//*[@id='app-navigation-vue']/div[1]"));
-        // clicking on add note
-        addNote.click();
-        // identifying the text area where to write the note
-        WebElement writeNote = driver.findElement(By.xpath("//*[@id='note-container']/div/div[2]"));
-        //Typing a message to the Note
-        writeNote.sendKeys("New Note", Keys.ENTER );
+
+        NotePage notePage = new NotePage();
+
+        notePage.addNotes();
+    }
+
+    @Test
+    public  void isDisplayed(){
+
+        NotePage notePage = new NotePage();
+
+      Assert.assertTrue(notePage.newNotesDisplayed());
     }
 
 }
