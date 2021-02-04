e6b9872ca75e297cb0cad2a85fe96000977a469d
umeda Davlatova <umeda.davlatova@gmail.com>
2021-02-03 21:52:53 -0500

    created Note test class and created Test. Created String expected Note title. Created a notesGetActualTitle in AllFilesPage class

--- src/test/java/com/trycloud/pages/AllFilesPage.java
+++ src/test/java/com/trycloud/pages/AllFilesPage.java
@@ -2,34 +2,45 @@ package com.trycloud.pages;
 
 import com.trycloud.pages.base.BasePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class AllFilesPage extends BasePage {
-    public String expectedTitleFiles="Files - Trycloud";
-    public String expectedTitleActivities="Activity - Trycloud";
-    public String expectedTitleGallery="Gallery - Trycloud";
-    public String expectedTitleTalk="Talk - Trycloud";
-    public String expectedTitleCalendar="Calendar - Trycloud";
-    public String expectedTitleDeck="Deck - Trycloud";
-    public String expectedTitleAnnouncements="Announcements - Trycloud";
+    public String expectedTitleFiles = "Files - Trycloud";
+    public String expectedTitleActivities = "Activity - Trycloud";
+    public String expectedTitleGallery = "Gallery - Trycloud";
+    public String expectedTitleTalk = "Talk - Trycloud";
+    public String expectedTitleCalendar = "Calendar - Trycloud";
+    public String expectedTitleDeck = "Deck - Trycloud";
+    public String expectedTitleAnnouncements = "Announcements - Trycloud";
+    public String expectedTitleNotes = "Notes - Trycloud - QA";
 
-    public String filesGetActualTitle(){
+    public String notesGetActualTitle() {
+        driver.findElement(By.xpath("//*[@id='appmenu']/li[7]")).click();
+
+        return driver.getTitle();
+    }
+
+    public String filesGetActualTitle() {
         driver.findElement(By.xpath("//body//div//a[@href='/index.php/apps/files/']")).click();
         return driver.getTitle();
     }
 
-    public String activityGetActualTitle(){
+    public String activityGetActualTitle() {
         driver.findElement(By.xpath("//body//div//a[@href='/index.php/apps/activity/']")).click();
         return driver.getTitle();
     }
-    public String galleryGetActualTitle(){
+
+    public String galleryGetActualTitle() {
         driver.findElement(By.xpath("//body//div//a[@href='/index.php/apps/gallery/']")).click();
         return driver.getTitle();
     }
-    public String talkGetActualTitle(){
+
+    public String talkGetActualTitle() {
         driver.findElement(By.xpath("//body//div//a[@href='/index.php/apps/spreed/']")).click();
         return driver.getTitle();
     }
-    public String calendarGetActualTitle(){
+
+    public String calendarGetActualTitle() {
         driver.findElement(By.xpath("//body//div//a[@href='/index.php/apps/calendar/']")).click();
         return driver.getTitle();
     }
---
+++ src/test/java/com/trycloud/tests/US_6_NotesModule.java
@@ -0,0 +1,49 @@
+package com.trycloud.tests;
+
+import com.trycloud.pages.AllFilesPage;
+import com.trycloud.tests.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class US_6_NotesModule extends TestBase {
+
+       @BeforeMethod
+
+       public void setUpMethod(){
+        super.setUpMethod();
+    }
+    @Test
+
+    public  void noteModuleTest(){
+        AllFilesPage notes = new AllFilesPage();
+        Assert.assertEquals(notes.notesGetActualTitle(),notes.expectedTitleNotes,"We dont have access to notes ");
+
+
+    }
+    @Test
+    public void noteAddTest(){
+        driver.findElement(By.xpath("//*[@id='appmenu']/li[7]")).click();
+        // identifying the add note button
+        WebElement addNote = driver.findElement(By.xpath("//*[@id='app-navigation-vue']/div[1]"));
+        // clicking on add note
+        addNote.click();
+        // identifying the text area where to write the note
+        WebElement writeNote = driver.findElement(By.xpath("//*[@id='note-container']/div/div[2]"));
+        //Typing a message to the Note
+        writeNote.sendKeys("New Note", Keys.ENTER );
+    }
+    @AfterMethod
+    public void tearDown() {
+        super.tearDown();
+    }
+
+
+
+
+
+}
