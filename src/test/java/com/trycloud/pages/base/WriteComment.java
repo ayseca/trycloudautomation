1d309621801a22ad9c57cc284b709a812da5303f
nilufaranwar <nilox_@hotmail.com>
2021-02-11 23:01:06 -0500

    US3,TC8 create new test case CommentDisplay

--- src/test/java/com/trycloud/pages/base/WriteComment.java
+++ src/test/java/com/trycloud/pages/base/WriteComment.java
@@ -1,4 +1,28 @@
 package com.trycloud.pages.base;
 
-public class WriteComment {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class WriteComment extends BasePage{
+    String fileModule = "//body//div//a[@href='/index.php/apps/files/']";
+    String actionIcon ="//*[@id='fileList']/tr[1]/td[2]/a/span[2]/a[2]";
+    String detail ="//*[@id='fileList']/tr[4]/td[2]/div/ul/li[4]/a";
+    String commentsTab ="//*[@id='commentsTabView']";
+    String commentBox ="//*[@id='commentsTabView']/div[1]/form/div[1]";
+    String commentDisplay="//*[@id='commentsTabView']/ul/li";
+
+
+    public void Comment(){
+        driver.findElement(By.xpath(fileModule)).click();
+        driver.findElement(By.xpath(actionIcon)).click();
+        driver.findElement(By.xpath(detail)).click();
+        driver.findElement(By.xpath(commentsTab)).click();
+        driver.findElement(By.xpath(commentBox)).sendKeys("good"+ Keys.ENTER);
+
+
+    }
+
+    public boolean CommentDisplayed(){
+        return driver.findElement(By.xpath(commentDisplay)).isDisplayed();
+    }
 }
---
+++ src/test/java/com/trycloud/tests/base/CommentDisplay.java
@@ -0,0 +1,4 @@
+package com.trycloud.tests.base;
+
+public class CommentDisplay {
+}
