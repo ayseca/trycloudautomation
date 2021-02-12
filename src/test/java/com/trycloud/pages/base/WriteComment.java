ef3206c179553c75e99b4e105e0e1efce8a1fef9
nilufaranwar <nilox_@hotmail.com>
2021-02-11 23:20:02 -0500

    US3,TC8 create new test case CommentDisplay and test passed

--- src/test/java/com/trycloud/pages/base/WriteComment.java
+++ src/test/java/com/trycloud/pages/base/WriteComment.java
@@ -6,11 +6,15 @@ import org.openqa.selenium.Keys;
 public class WriteComment extends BasePage{
     String fileModule = "//body//div//a[@href='/index.php/apps/files/']";
     String actionIcon ="//*[@id='fileList']/tr[1]/td[2]/a/span[2]/a[2]";
-    String detail ="//*[@id='fileList']/tr[4]/td[2]/div/ul/li[4]/a";
+    String detail ="//*[@id='fileList']/tr[1]/td[2]/div/ul/li[4]/a";
     String commentsTab ="//*[@id='commentsTabView']";
     String commentBox ="//*[@id='commentsTabView']/div[1]/form/div[1]";
     String commentDisplay="//*[@id='commentsTabView']/ul/li";
 
+    //*[@id="fileList"]/tr[1]/td[2]/a/span[2]/a[2]
+    //*[@id="fileList"]/tr[1]/td[2]/div/ul/li[4]/a
+    //*[@id="commentsTabView"]
+    ////*[@id="commentsTabView"]/div[1]/form/div[1]
 
     public void Comment(){
         driver.findElement(By.xpath(fileModule)).click();
--- src/test/java/com/trycloud/tests/base/CommentDisplay.java
+++ src/test/java/com/trycloud/tests/base/CommentDisplay.java
@@ -1,4 +1,16 @@
 package com.trycloud.tests.base;
 
-public class CommentDisplay {
+import com.trycloud.pages.base.WriteComment;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CommentDisplay extends TestBase {
+
+    @Test
+    public void  commentDisplay(){
+        WriteComment writeComment = new WriteComment();
+        writeComment.Comment();
+        Assert.assertTrue(writeComment.CommentDisplayed());
+    }
+
 }
