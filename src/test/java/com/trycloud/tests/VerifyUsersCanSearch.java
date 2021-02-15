32656cd88e19886214f8993bd92e58591e158fb0
maryam <maryam.e.mahmood@gmail.com>
2021-02-15 17:54:38 -0600

    2/15 Commet

--- src/test/java/com/trycloud/tests/VerifyUsersCanSearch.java
+++ src/test/java/com/trycloud/tests/VerifyUsersCanSearch.java
@@ -1,6 +1,7 @@
 package com.trycloud.tests;
 
 import com.trycloud.pages.HomePage;
+import com.trycloud.pages.searchFunctionality;
 import com.trycloud.tests.base.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,16 +13,16 @@ import org.testng.annotations.Test;
 public class VerifyUsersCanSearch extends TestBase {
 
     @Test
-    public void verify_search() {
-        //search any thing
-        WebElement searchItem = driver.findElement(By.id("searchbox"));
-        searchItem.sendKeys("user1", Keys.ENTER);
-        // Verify the app displays the expected result option.
-        String expectedResult = "user1";
-        WebElement searchWindow = driver.findElement(By.xpath("//h2[@class='empty-content__title']"));
-        String actualResult = searchWindow.getText();
-        Assert.assertTrue(actualResult.contains(expectedResult));
-        //....
+    public void verify_search() throws InterruptedException {
+
+        searchFunctionality verifySearch = new searchFunctionality();
+        Thread.sleep(2000);
+        verifySearch.searchIcon.click();
+        String searchValue = "user13";
+        Thread.sleep(2000);
+        verifySearch.searchBar.sendKeys(searchValue + Keys.ENTER);
+        Thread.sleep(2000);
+        Assert.assertTrue(driver.findElement(By.xpath("//h3[@class='unified-search__result-line-one']")).isDisplayed());
     }
 
 
