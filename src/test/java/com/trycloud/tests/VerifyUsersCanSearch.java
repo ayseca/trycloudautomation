5ed52b765f0af1d9e1f70f806045f8a162ccdf62
maryam <maryam.e.mahmood@gmail.com>
2021-02-15 18:22:01 -0600

    2/15 Committed

--- src/test/java/com/trycloud/tests/VerifyUsersCanSearch.java
+++ src/test/java/com/trycloud/tests/VerifyUsersCanSearch.java
@@ -1,6 +1,7 @@
 package com.trycloud.tests;
 
 import com.trycloud.pages.HomePage;
+import com.trycloud.pages.searchFunctionality;
 import com.trycloud.tests.base.TestBase;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.By;
@@ -16,17 +17,22 @@ public class VerifyUsersCanSearch extends TestBase {
     public void verify_search() throws InterruptedException {
         //search any thing
 
-
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("svg[class='material-design-icon__svg']")).click();
-        Thread.sleep(2000);
-        WebElement searchItem = driver.findElement(By.xpath("//input[@class='unified-search__form-input']"));
-        String searchValue = "readme";
-        searchItem.sendKeys(searchValue + Keys.ENTER);
+        searchFunctionality verifySearch= new searchFunctionality();
+        verifySearch.searchIcon.click();
+        String searchValue = "user13";
+        verifySearch.searchBar.sendKeys(searchValue+Keys.ENTER);
 
 
-Assert.assertTrue(driver.findElement(By.xpath("//h3[@class='unified-search__result-line-one']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//h3[@class='unified-search__result-line-one']")).isDisplayed());
     }
 
 
 }
+/*
+    Thread.sleep(2000);
+        driver.findElement(By.cssSelector("svg[class='material-design-icon__svg']")).click();
+        Thread.sleep(2000);
+        WebElement searchItem = driver.findElement(By.xpath("//input[@class='unified-search__form-input']"));
+        String searchValue = "readme";
+
+ */
